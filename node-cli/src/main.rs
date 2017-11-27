8882d9401bdb215afd942bae2d0cf0db0feca16c
Raphael Zimmermann <mail@raphael.li>
2017-11-27 14:40:40 +0100

    Extend config with known nodes

--- node-cli/src/main.rs
+++ node-cli/src/main.rs
@@ -13,6 +13,17 @@ fn main() {
         .version(crate_version!())
         .author(crate_authors!())
         .arg(
+            Arg::with_name("known-node")
+                .short("k")
+                .long("known-node")
+                .multiple(true)
+                .number_of_values(1)
+                .takes_value(true)
+                .help(
+                    "ip address and port (<ip-address> <port>) of other known nodes in the network",
+                ),
+        )
+        .arg(
             Arg::with_name("ip")
                 .help("IP to bind")
                 .default_value("0.0.0.0"),
@@ -38,8 +49,13 @@ fn main() {
     let port = matches.value_of("port").unwrap();
     let storage_dir = matches.value_of("storage-dir").unwrap();
     let db_file = matches.value_of("db-file").unwrap();
+    let known_nodes = matches
+        .values_of("known-node")
+        .unwrap_or_default()
+        .map(|v| v.to_owned())
+        .collect::<Vec<_>>();
 
-    let conf = Config::new(ip, port, storage_dir, db_file).unwrap_or_else(|err| {
+    let conf = Config::new(ip, port, storage_dir, db_file, known_nodes).unwrap_or_else(|err| {
         match err {
             ParseError::InvalidIp(err) => eprintln!("The given IP could not be parsed ({})", err),
             ParseError::InvalidPort(err) => {
